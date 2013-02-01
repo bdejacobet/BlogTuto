4d8025da56e317fb30842b370de1719bd42de4ea
benijaco <benijaco@benijaco-VirtualBox.(none)>
2013-02-01 16:22:54 +0100

    mise en place des routes avec un prefixe /blog

--- src/Benijaco/BlogBundle/Controller/BlogController.php
+++ src/Benijaco/BlogBundle/Controller/BlogController.php
@@ -3,11 +3,17 @@
 namespace Benijaco\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends Controller
 {
     public function indexAction()
     {
         return $this->render('BenijacoBlogBundle:Blog:index.html.twig', array());
+    }
+    
+    public function voirAction($id)
+    {
+        return new Response("Affichage de l'article d'id : ".$id.".");
     }
 }
