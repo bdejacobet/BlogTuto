1750c3e5de02b2f92497600fb124d3e6854a388d
benijaco <benijaco@benijaco-VirtualBox.(none)>
2013-02-04 18:04:03 +0100

    mise en place des methodes dans le controleur

--- src/Benijaco/BlogBundle/Controller/BlogController.php
+++ src/Benijaco/BlogBundle/Controller/BlogController.php
@@ -9,26 +9,56 @@ class BlogController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BenijacoBlogBundle:Blog:index.html.twig', array());
+        // On ne sait pas combien de pages il y a
+        // Mais on sait qu'une page doit être supérieure ou égale à 1
+        if( $page < 1 ) {
+          // On déclenche une exception NotFoundHttpException
+          // Cela va afficher la page d'erreur 404 (on pourra personnaliser cette page plus tard, d'ailleurs)
+          throw $this->createNotFoundException('Page inexistante (page = '.$page.')');
+        }
+
+        // Ici, on récupérera la liste des articles, puis on la passera au template
+
+        // Mais pour l'instant, on ne fait qu'appeler le template
+        return $this->render('BenijacoBlogBundle:Blog:index.html.twig');
     }
     
     public function voirAction($id)
     {
-        return new Response("Affichage de l'article d'id : ".$id.".");
+        return $this->render('BenijacoBlogBundle:Blog:voir.html.twig', array('id' => $id));
     }
     
     public function ajouterAction()
-    {
-        return new Response("Ajout d'un article");
+    { 
+        // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+        if( $this->get('request')->getMethod() == 'POST' ){
+          // Ici, on s'occupera de la création et de la gestion du formulaire
+
+          $this->get('session')->getFlashBag()->add('notice', 'Article bien enregistré');
+
+          // Puis on redirige vers la page de visualisation de cet article
+          return $this->redirect( $this->generateUrl('benijacoblog_voir', array('id' => 5)) );
+        }
+
+        // Si on n'est pas en POST, alors on affiche le formulaire
+        return $this->render('BenijacoBlogBundle:Blog:ajouter.html.twig');
     }
     
     public function modifierAction($id)
     {
-        return new Response("Modification de l'article d'id : ".$id.".");
+        // Ici, on récupérera l'article correspondant à l'$id
+
+        // Ici, on s'occupera de la création et de la gestion du formulaire
+
+        return $this->render('BenijacoBlogBundle:Blog:modifier.html.twig');
     }
-    
+
     public function supprimerAction($id)
     {
-        return new Response("Suppression de l'article d'id : ".$id.".");
+        // Ici, on récupérera l'article correspondant à l'$id
+
+        // Ici, on gérera la suppression de l'article en question
+
+        return $this->render('BenijacoBlogBundle:Blog:supprimer.html.twig');
     }
 }
