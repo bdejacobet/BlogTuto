ed4c7af70c526d0178b9242508277b1b2e2afa0a
benijaco <benijaco@benijaco-VirtualBox.(none)>
2013-02-05 12:23:05 +0100

    mise en place des views

--- src/Benijaco/BlogBundle/Controller/BlogController.php
+++ src/Benijaco/BlogBundle/Controller/BlogController.php
@@ -9,48 +9,73 @@ class BlogController extends Controller
 {
     public function indexAction()
     {
-        // On ne sait pas combien de pages il y a
-        // Mais on sait qu'une page doit être supérieure ou égale à 1
-        if( $page < 1 ) {
-          // On déclenche une exception NotFoundHttpException
-          // Cela va afficher la page d'erreur 404 (on pourra personnaliser cette page plus tard, d'ailleurs)
-          throw $this->createNotFoundException('Page inexistante (page = '.$page.')');
+
+        if( isset($page) && $page < 1 ) {
+            throw $this->createNotFoundException('Page inexistante (page = '.$page.')');
         }
 
-        // Ici, on récupérera la liste des articles, puis on la passera au template
+        // Ici, on récupérera la liste des articles
+        $articles = array(
+          array('titre' => 'Mon weekend a Phi Phi Island !',          'id' => 1, 'auteur' => 'winzou',  'contenu' => 'Ce weekend était trop bien. Blabla…',  'date' => new \Datetime()),
+          array('titre' => 'Repetition du National Day de Singapour', 'id' => 2, 'auteur' => 'winzou',  'contenu' => 'Bientôt prêt pour le jour J. Blabla…', 'date' => new \Datetime()),
+          array('titre' => 'Chiffre d\'affaire en hausse',            'id' => 3, 'auteur' => 'M@teo21', 'contenu' => '+500% sur 1 an, fabuleux. Blabla…',    'date' => new \Datetime())
+        );     
 
-        // Mais pour l'instant, on ne fait qu'appeler le template
-        return $this->render('BenijacoBlogBundle:Blog:index.html.twig');
+        return $this->render('BenijacoBlogBundle:Blog:index.html.twig', array(
+            'articles' => $articles
+        ));
     }
     
     public function voirAction($id)
     {
-        return $this->render('BenijacoBlogBundle:Blog:voir.html.twig', array('id' => $id));
+        
+        // Ici, on récupérera les données de l'articles
+        $article = array(
+            'id'      => 1,
+            'titre'   => 'Mon weekend a Phi Phi Island !',
+            'auteur'  => 'winzou',
+            'contenu' => 'Ce weekend était trop bien. Blabla…',
+            'date'    => new \Datetime()
+        );
+          
+        return $this->render('BenijacoBlogBundle:Blog:voir.html.twig', array(
+            'article' => $article
+        ));
     }
     
     public function ajouterAction()
     { 
-        // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+
+        // si POST : ajout article
         if( $this->get('request')->getMethod() == 'POST' ){
-          // Ici, on s'occupera de la création et de la gestion du formulaire
+            // Ici, on s'occupera de la création et de la gestion du formulaire
 
-          $this->get('session')->getFlashBag()->add('notice', 'Article bien enregistré');
+            // message flash pour confirmer ajout article
+            $this->get('session')->getFlashBag()->add('notice', 'Article bien enregistré');
 
-          // Puis on redirige vers la page de visualisation de cet article
-          return $this->redirect( $this->generateUrl('benijacoblog_voir', array('id' => 5)) );
+            // redirection vers la page de visualisation de cet article
+            return $this->redirect( $this->generateUrl('benijacoblog_voir', array('id' => 5)) );
         }
 
-        // Si on n'est pas en POST, alors on affiche le formulaire
+        // si on n'est pas en POST : affichage du formulaire
         return $this->render('BenijacoBlogBundle:Blog:ajouter.html.twig');
     }
     
     public function modifierAction($id)
     {
-        // Ici, on récupérera l'article correspondant à l'$id
+        
+        // Ici, on récupérera les données de l'articles
+        $article = array(
+            'id'      => 1,
+            'titre'   => 'Mon weekend a Phi Phi Island !',
+            'auteur'  => 'winzou',
+            'contenu' => 'Ce weekend était trop bien. Blabla…',
+            'date'    => new \Datetime()
+        );
 
-        // Ici, on s'occupera de la création et de la gestion du formulaire
-
-        return $this->render('BenijacoBlogBundle:Blog:modifier.html.twig');
+        return $this->render('BenijacoBlogBundle:Blog:modifier.html.twig', array(
+            'article' => $article
+        ));
     }
 
     public function supprimerAction($id)
@@ -60,5 +85,19 @@ class BlogController extends Controller
         // Ici, on gérera la suppression de l'article en question
 
         return $this->render('BenijacoBlogBundle:Blog:supprimer.html.twig');
+    }
+    
+    public function menuAction()
+    {
+        // Ici on fixe en dur une liste, bien entendu par la suite on la récupérera depuis la BDD !
+        $liste = array(
+          array('id' => 2, 'titre' => 'Mon dernier weekend !'),
+          array('id' => 5, 'titre' => 'Sortie de Symfony2.1'),
+          array('id' => 9, 'titre' => 'Petit test')
+        );
+
+        return $this->render('BenijacoBlogBundle:Blog:menu.html.twig', array(
+          'liste_articles' => $liste 
+        ));
     }
 }
