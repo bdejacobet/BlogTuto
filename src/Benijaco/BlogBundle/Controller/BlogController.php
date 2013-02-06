00ef274a48e069b1cb78149c37397636f1f05b9e
Benoit de Jacobet <benijaco@gmail.com>
2013-02-06 17:00:30 +0100

    mise en place de doctrine et des entities article et image

--- src/Benijaco/BlogBundle/Controller/BlogController.php
+++ src/Benijaco/BlogBundle/Controller/BlogController.php
@@ -5,6 +5,9 @@ namespace Benijaco\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+use Benijaco\BlogBundle\Entity\Article;
+use Benijaco\BlogBundle\Entity\Image;
+
 class BlogController extends Controller
 {
     public function indexAction()
@@ -29,14 +32,13 @@ class BlogController extends Controller
     public function voirAction($id)
     {
         
-        // Ici, on récupérera les données de l'articles
-        $article = array(
-            'id'      => 1,
-            'titre'   => 'Mon weekend a Phi Phi Island !',
-            'auteur'  => 'winzou',
-            'contenu' => 'Ce weekend était trop bien. Blabla…',
-            'date'    => new \Datetime()
-        );
+        // On récupère le repository
+        $repository = $this->getDoctrine()
+                            ->getManager()
+                            ->getRepository('BenijacoBlogBundle:Article');
+
+        // On récupère l'entité correspondant à l'id $id
+        $article = $repository->find($id);
           
         return $this->render('BenijacoBlogBundle:Blog:voir.html.twig', array(
             'article' => $article
@@ -45,16 +47,38 @@ class BlogController extends Controller
     
     public function ajouterAction()
     { 
+        
+        // Création de l'entité
+        $article = new Article();
+        $article->setTitre('Mon dernier weekend');
+        $article->setAuteur('Bibi');
+        $article->setContenu("C'était vraiment super et on s'est bien amusé.");
+
+        // Création de l'entité Image
+        $image = new Image();
+        $image->setUrl('http://uploads.siteduzero.com/icones/478001_479000/478657.png');
+        $image->setAlt('Logo Symfony2');
+
+        // On lie l'image à l'article
+        $article->setImage($image);
+
+        // On récupére l'EntityManager
+        $em = $this->getDoctrine()->getManager();
+
+        // Etape 1 : On « persiste » l'entité
+        $em->persist($article);
+
+        // Etape 2 : On « flush » tout ce qui a été persisté avant
+        $em->flush();
 
         // si POST : ajout article
         if( $this->get('request')->getMethod() == 'POST' ){
-            // Ici, on s'occupera de la création et de la gestion du formulaire
 
             // message flash pour confirmer ajout article
             $this->get('session')->getFlashBag()->add('notice', 'Article bien enregistré');
 
             // redirection vers la page de visualisation de cet article
-            return $this->redirect( $this->generateUrl('benijacoblog_voir', array('id' => 5)) );
+            return $this->redirect( $this->generateUrl('benijacoblog_voir', array('id' => $article->getId())) );
         }
 
         // si on n'est pas en POST : affichage du formulaire
