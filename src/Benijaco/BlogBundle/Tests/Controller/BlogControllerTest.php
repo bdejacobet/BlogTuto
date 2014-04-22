78429ee5edb1e143158f2ec81fdfc160c9dcb008
Benoit DE JACOBET <benoit.dejacobet@smile.fr>
2014-04-22 12:18:23 +0200

    maj pour test travis

---
+++ src/Benijaco/BlogBundle/Tests/Controller/BlogControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Benijaco\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testSimple()
+    {
+        $this->assertEquals(0, 0);
+    }
+}
